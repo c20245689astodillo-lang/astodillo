1f0db424c965f7a655eb7cb3383c8570f966d788
c20245689astodillo-lang <c20245689.astodillo@csav.edu.ph>
2026-09-04 14:43:27 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log("Hello, World.");
 console.log("From script.js file.");
+
+const myName = "Raijen";
+let age = 20;
+let myNumber = 9386477995;
+const address = "Saravia";
+
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${myNumber}`);
+console.log(`Address: ${address}`); 
+console.log(` ${67}`);
